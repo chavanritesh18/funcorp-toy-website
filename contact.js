db7ce4cbee790c05daf35ddb11e9f1dcaf4216e4
Chavan Ritesh <140919594+chavanritesh18@users.noreply.github.com>
2024-01-21 13:32:11 +0530

    files uploaded

---
+++ contact.js
@@ -0,0 +1,10 @@
+function connect(){
+    let name = document.getElementById("names");
+    let num = document.getElementById("number");
+    if(name.value == "" && num.value == ""){
+        alert("Fill Details")
+    }else{
+        alert("Thanks For Connecting")
+    }
+
+}
